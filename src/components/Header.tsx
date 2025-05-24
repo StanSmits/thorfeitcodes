bd05c775095326ba079a03caea99bc645ef3dfbf
Stan <s1136644@student.hsleiden.nl>
2025-05-24 21:03:36 +0200

    remove last update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,6 @@ import React from "react";
 import AmsterdamLogo from "./AmsterdamLogo";
 
 const Header: React.FC = () => {
-  const timeLastUpdate = new Date().toLocaleString("nl-NL", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  });
-
   return (
     <header className="bg-white shadow-sm">
       <div className="container mx-auto px-4 py-2">
@@ -16,14 +10,9 @@ const Header: React.FC = () => {
           onClick={() => (window.location.href = "/")}
           role="button"
         >
-          <div className="w-6 h-6">
-            {/* <AmsterdamLogo /> */}
-          </div>
+          <div className="w-6 h-6">{/* <AmsterdamLogo /> */}</div>
           <div>
             <h1 className="text-base font-bold">Feitcode Tool</h1>
-            <p className="text-xs text-gray-600">
-              Laatste update: {timeLastUpdate}
-            </p>
           </div>
           <div className="ml-6 hover:col-red-600">
             <a
